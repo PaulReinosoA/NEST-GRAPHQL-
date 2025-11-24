026063817603c8eec490765faba75fd0e8d7614f
REINOSOW <wreinoso@solidario.fin.ec>
2025-11-24 17:54:23 -0500

    100 relacion entre item y user

--- 03-anylist/src/app.module.ts
+++ 03-anylist/src/app.module.ts
@@ -34,14 +34,14 @@ import { AuthModule } from './auth/auth.module';
     // }),
 
     // //todo:configuracion basica
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      // debug: false,
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      includeStacktraceInErrorResponses: false, //deshabilitarlo  stacktrace
-    }),
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   // debug: false,
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    //   playground: false,
+    //   plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    //   includeStacktraceInErrorResponses: false, //deshabilitarlo  stacktrace
+    // }),
     TypeOrmModule.forRoot({
       //bdd postgres
       type: 'postgres',
--- 03-anylist/src/main.ts
+++ 03-anylist/src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TrackingIdInterceptor } from './interceptors/tracking.interceptor';
+/*para la configuracion de carga y configuracion de modulo asycn del app.module*/
+// import crypto from 'crypto';
+// // Polyfill para que @nestjs/graphql encuentre crypto
+// (global as any).crypto = crypto;
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
