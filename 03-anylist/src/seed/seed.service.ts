56e92b40fd3d361ef569af439c537f7c7396ba72
PaulReinosoA <164814520+PaulReinosoA@users.noreply.github.com>
2025-11-25 23:38:52 -0500

    113 - borrar registros anteriores

--- 03-anylist/src/seed/seed.service.ts
+++ 03-anylist/src/seed/seed.service.ts
@@ -1,19 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from 'src/items/entities/item.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
-  constructor() {}
+  private isProd: boolean;
+
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(Item)
+    private readonly itemRepository: Repository<Item>,
+
+    @InjectRepository(User)
+    private readonly usesrRepository: Repository<User>,
+  ) {
+    this.isProd = this.configService.get('STATE') === 'prod';
+  }
 
   async executeSeed() {
-
     //todo: limpiar DB
-
+    if (this.isProd) {
+      throw new Error('No se puede ejecutar el seed en produccion');
+    }
+    this.deleteDatabase();
 
     //todo: construir usuarios
 
-    
     //todo: construir items
 
     return true;
+  }
+
+  async deleteDatabase() {
+    await this.itemRepository.createQueryBuilder().delete().where({}).execute();
+    await this.usesrRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
   }
 }
