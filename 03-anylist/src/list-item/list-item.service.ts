8011d0237505b7bc38604fec2fe20fbdfd1343e5
PaulReinosoA <164814520+PaulReinosoA@users.noreply.github.com>
2025-12-02 22:45:17 -0500

    informacion de los items - conteo

---
+++ 03-anylist/src/list-item/list-item.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { CreateListItemInput } from './dto/create-list-item.input';
+import { UpdateListItemInput } from './dto/update-list-item.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ListItem } from './entities/list-item.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ListItemService {
+  constructor(
+    @InjectRepository(ListItem)
+    private readonly listItemRepository: Repository<ListItem>,
+  ) {}
+
+  create(createListItemInput: CreateListItemInput) {
+    const { itemId, listId, ...rest } = createListItemInput;
+    const newListItem = this.listItemRepository.create({
+      ...rest,
+      item: { id: itemId },
+      list: { id: listId },
+    });
+    return this.listItemRepository.save(newListItem);
+  }
+
+  findAll() {
+    return `This action returns all listItem`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} listItem`;
+  }
+
+  update(id: number, updateListItemInput: UpdateListItemInput) {
+    return `This action updates a #${id} listItem`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} listItem`;
+  }
+}
--- 03-anylist/src/lists/entities/list.entity.ts
+++ 03-anylist/src/lists/entities/list.entity.ts
@@ -1,7 +1,15 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql';
 import { IsString } from 'class-validator';
+import { ListItem } from 'src/list-item/entities/list-item.entity';
 import { User } from 'src/user/entities/user.entity';
-import { Column, Entity, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @ObjectType()
 @Entity({ name: 'list' })
@@ -15,8 +23,12 @@ export class List {
   @IsString()
   name: string;
 
-  @ManyToOne(() => User, (user) => user.list, { nullable: false, lazy: true})  
+  @ManyToOne(() => User, (user) => user.list, { nullable: false, lazy: true })
   @Index('userIdList-index')
   @Field(() => User)
   user: User;
+
+  @OneToMany(() => ListItem, (listItem) => listItem.list)
+  @Field(() => [ListItem])
+  listItems: ListItem[];
 }
